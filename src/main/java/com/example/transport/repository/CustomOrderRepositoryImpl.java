dce41f16d4dc27710148818fe226cc0d1a4df90e
Łukasz Samul <samul.luk@gmail.com>
2018-11-13 20:00:53 +0100

    some changes

--- src/main/java/com/example/transport/repository/CustomOrderRepositoryImpl.java
+++ src/main/java/com/example/transport/repository/CustomOrderRepositoryImpl.java
@@ -18,59 +18,48 @@ public class CustomOrderRepositoryImpl implements CustomOrderRepository {
 	@Override
 	public double calculate(String sourceAddress, String destinationAddress) {
 
-		String key = "XXX";
-		String s = String.format(
-				"http://www.mapquestapi.com/directions/v2/routematrix?key=%s",
-				key);
-		
-		ObjectMapper mapper = new ObjectMapper();
-		
-		
-		
-		
-		
-		
-		// wysłać JSON
-		
-		// odczytać odebraną odpowiedź
-		
-		
-//		ObjectNode locationsJson = mapper.createObjectNode();
-//
-//		ArrayNode locationsArray = mapper.createArrayNode();
-//		locationsArray.add(sourceAddress + ", Polska");
-//		locationsArray.add(destinationAddress + ", Polska");
-//
-//		locationsJson.putPOJO("locations", locationsArray);
+		String key = "xxx";
+		String s = String.format("http://www.mapquestapi.com/directions/v2/routematrix?key=%s", key);
 
-//		System.out.println(locationsJson.toString());
+		ObjectMapper mapper = new ObjectMapper();
+
+		// wysłać JSON
+
+		// odczytać odebraną odpowiedź
+
+		// ObjectNode locationsJson = mapper.createObjectNode();
+		//
+		// ArrayNode locationsArray = mapper.createArrayNode();
+		// locationsArray.add(sourceAddress + ", Polska");
+		// locationsArray.add(destinationAddress + ", Polska");
+		//
+		// locationsJson.putPOJO("locations", locationsArray);
+
+		// System.out.println(locationsJson.toString());
 
 		try {
-			String temp = String.format(
-					"{ \"locations\": [%s+ \", Polska\",%s+ \", Polska\"] }",
-					sourceAddress, destinationAddress);
+			String temp = String.format("{ \"locations\": [%s+ \", Polska\",%s+ \", Polska\"] }", sourceAddress,
+					destinationAddress);
 			JSONObject locationsJson = new JSONObject(temp);
-			
-			
+
 			URL url = new URL(s);
 			HttpURLConnection yc = (HttpURLConnection) url.openConnection();
 			yc.setRequestMethod("GET");
 			yc.connect();
 
 			Scanner scan = new Scanner(url.openStream());
-		    String str = new String();
-		    while (scan.hasNext())
-		        str += scan.nextLine();
-		    scan.close();
-		    
-//			BufferedReader sb = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-		    JSONObject json = new JSONObject(str);
-			
-			
+			String str = new String();
+			while (scan.hasNext())
+				str += scan.nextLine();
+			scan.close();
+
+			// BufferedReader sb = new BufferedReader(new
+			// InputStreamReader(yc.getInputStream()));
+			JSONObject json = new JSONObject(str);
 
 			double distance = json.getJSONArray("distance").getDouble(1);
-			
-			//sb.close();
+
+			// sb.close();
 			return distance;
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
