b08efbb7109e812022599a8e89145560920e6505
Łukasz Samul <samul.luk@gmail.com>
2018-12-17 19:06:19 +0100

    some changes

--- src/main/java/com/example/transport/model/Way.java
+++ src/main/java/com/example/transport/model/Way.java
@@ -1,5 +1,16 @@
 package com.example.transport.model;
 
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+@Data
+@Entity
 public class Way {
 
+	private double[] distance;
+	
 }
